1604478ce45b649d8f8283601c4408e18c7b0206
Mikhail Tyo <mikhail.tyolab@gmail.com>
2023-11-14 19:10:03 +0300

    test: add tests

---
+++ rectangle_test.py
@@ -0,0 +1,30 @@
+import unittest
+
+from rectangle import area
+from rectangle import perimeter
+
+class MyTestCase(unittest.TestCase):
+    def test_area_zero(self):
+        res = area(0, 0)
+        self.assertEqual(res, 0)
+
+    def test_area_small_input(self):
+        res = area(2, 5)
+        self.assertEqual(res, 10)
+
+    def test_area_big_input(self):
+        res = area(123456789, 987654321)
+        self.assertEqual(res, 121932631112635269)
+
+    def test_perimeter_zero(self):
+        res = perimeter(0, 0)
+        self.assertEqual(res, 0)
+
+    def test_perimeter_small_input(self):
+        res = perimeter(2, 5)
+        self.assertEqual(res, 14)
+
+    def test_perimeter_big_input(self):
+        res = perimeter(123456789, 987654321)
+        self.assertEqual(res, 2222222220)
+
